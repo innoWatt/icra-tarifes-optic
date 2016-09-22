6b4d71b3ace73d6ead89cc7e3ae57d70d2a2ee6a
lbosch <lbosch@icra.cat>
2016-09-22 18:51:04 +0100

    per fer inici sessio nomes cal una trama

--- usuari.py
+++ usuari.py
@@ -23,15 +23,13 @@ import pregunta as P
 #ESTIC FENT PROVES AMB ASDU 122. Només funciona direccio de registre=21 :(
 d=48915; psw=1 #direccio comptador i password
 
-P.pregunta(C.creaTramaFix(0b01000000,d)) #request link reset
+#P.pregunta(C.creaTramaFix(0b01000000,d)) #request link reset
 P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU183(psw))) #request user data & send password
 P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
 P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU122(21,1,2,C.creaTemps(20,5,16,0,0),C.creaTemps(23,5,16,0,0))))
 P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
-P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
-P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
-'''
-P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
-P.pregunta(C.creaTramaVar(0b01010011,d,C.creaASDU187())) #request end session
-P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
-'''
+#P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
+#P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
+#P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
+#P.pregunta(C.creaTramaVar(0b01010011,d,C.creaASDU187())) #request end session
+#P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
