667a9b367b8893c75d2b3745ccd9027d725a2446
lbosch <lbosch@icra.cat>
2016-09-23 03:14:37 +0200

    prova detecta errors

--- usuari.py
+++ usuari.py
@@ -27,17 +27,22 @@ d=1; psw=1 #direccio comptador=1 i password=1
 P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU183(psw))) #request user data & send password
 P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
 
-P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU115(11,1,2,C.creaTemps(20,5,16,0,0))))
+P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU122(21,1,2,C.creaTemps(21,5,16,0,0),C.creaTemps(23,5,16,0,0))))
+P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
+P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
+P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
+P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
+P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
+P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
+P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
+P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
 P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
 
-#P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU122(21,1,2,C.creaTemps(20,5,16,0,0),C.creaTemps(23,5,16,0,0))))
-#P.pregunta(C.creaTramaVar(0x73,1,C.creaASDU101(11,C.creaTemps(25,1,16,6,5),C.creaTemps(25,1,16,8,5))))
-#P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
-#P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
-#P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
-#P.pregunta(C.creaTramaFix(0b01011011,d)) #request class 2 data
 #P.pregunta(C.creaTramaVar(0b01010011,d,C.creaASDU187())) #request end session
 #P.pregunta(C.creaTramaFix(0b01111011,d)) #request class 2 data (flip FCB)
+
+#P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU115(11,1,2,C.creaTemps(20,5,16,0,0)))) # asdu no disponible!
+#P.pregunta(C.creaTramaVar(0x73,1,C.creaASDU101(11,C.creaTemps(25,1,16,6,5),C.creaTemps(25,1,16,8,5))))
 
 #prova asdu 101
 #P.pregunta(C.creaTramaVar(0b01110011,d,C.creaASDU101(11)))
