c716e0b562d73c1bcb00ff1972dae7046105f727
lbosch <lbosch@icra.cat>
2017-05-12 23:21:42 +0200

    canvi menu ppal

--- inici.sh
+++ inici.sh
@@ -4,6 +4,9 @@ cd usr #move pwd (important for python import paths)
 
 options=("test" "hora" "instantani" "dia" "mes_actual" "historic" "sortir")
 
+echo "+----------------+"
+echo "| MENÚ PRINCIPAL |"
+echo "+----------------+"
 echo "Tria una opció"
 
 select opt in "${options[@]}"
